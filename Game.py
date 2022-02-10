944f6091a12a10c16565308843d9fe2ec45e4820
Znarfen <97677302+Znarfen@users.noreply.github.com>
2022-02-10 10:21:14 +0100

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,23 +1,8 @@
-class Game:
+import requests
 
-    def __init__(self, name, console):
+response = requests.get("https://www.pricecharting.com/api/products?t=c0b53bce27c1bdab90b1605249e600dc43dfd1d5&q=Super Mario nds")
+response = response.json()
+game = response['product-name']
+platform = response['console-name']
 
-        self.name = name
-        self.console = console
-        
-
-
-    def Search(self):
-
-        from urllib import response
-        from aiohttp import request
-        import requests
-
-        response = requests.get("https://www.pricecharting.com/api/products?t=c0b53bce27c1bdab90b1605249e600dc43dfd1d5&q=Super Mario nds")
-        response = response.json()
-
-        print(response)
-
-G = Game("Super mario", "nds")
-G.Search
-
+print(game, platform)
